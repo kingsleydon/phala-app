5c4d3483136ce604ca3e2c1dbe124dff6a36e1f9
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-12-14 16:57:35 +0800

    pc3 test

--- apps/app/lib/graphql.ts
+++ apps/app/lib/graphql.ts
@@ -2,7 +2,7 @@ import {GraphQLClient} from 'graphql-request'
 import {getSdk} from './subsquidSdk'
 
 export const subsquidClient = new GraphQLClient(
-  process.env.NEXT_PUBLIC_SUBSQUID_URL ??
-    'https://squid.subsquid.io/khala-test/graphql'
+  // process.env.NEXT_PUBLIC_SUBSQUID_URL ??
+  'https://squid.subsquid.io/khala-test/v/pc3/graphql'
 )
 export const subsquidSdk = getSdk(subsquidClient)
